dab2b2b8e64cced2789e65687f949d60258cb4bf
Carolina Negrin <caro_negrin@hotmail.com>
2023-07-11 12:02:45 -0300

    modificación en aplicacion clientes

--- project/apps/cliente/admin.py
+++ project/apps/cliente/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from django.db import models
+from .models import Cliente, Domicilio, Ciudad
 # Register your models here.
+
+admin.site.register(Cliente)
+admin.site.register(Domicilio)
+admin.site.register(Ciudad)
--- project/apps/cliente/views.py
+++ project/apps/cliente/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    clientes_registros = Cliente.objects.all()
+    contexto = {"clientes": clientes_registros}
+    return render(request, "cliente/index.html", contexto)
